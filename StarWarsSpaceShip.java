d60ac7274c93cd652b99931c82582c96f3f3a0cb
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-04-12 17:15:36 -0400

    Delete StarWarsSpaceShip.java

--- StarWarsSpaceShip.java
+++
@@ -1,9 +0,0 @@
-package ExamQ6;
-
-public class StarWarsSpaceShip extends SpaceShip {
-	public StarWarsSpaceShip (int tonnageAmount, String newName, String franchiseName) {
-		String name = newName;
-		int tonnage = tonnageAmount;
-		String Franchise = franchiseName;
-	}
-}
