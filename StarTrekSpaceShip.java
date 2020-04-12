3f4c2ce1c074f31584676633ee8145be67bb7c86
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-04-12 17:15:10 -0400

    Delete StarTrekSpaceShip.java

--- StarTrekSpaceShip.java
+++
@@ -1,9 +0,0 @@
-package ExamQ6;
-
-public class StarTrekSpaceShip extends SpaceShip {
-	public StarTrekSpaceShip (int tonnageAmount, String newName, String franchiseName) {
-		String name = newName;
-		int tonnage = tonnageAmount;
-		String Franchise = franchiseName; 
-	}
-}
