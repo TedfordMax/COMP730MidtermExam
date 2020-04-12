7c0d8bab07f81e68e47ea81346d2b317b869454e
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-04-12 17:14:00 -0400

    Delete OtherSciFiSpaceShip.java

--- OtherSciFiSpaceShip.java
+++
@@ -1,9 +0,0 @@
-package ExamQ6;
-
-public class OtherSciFiSpaceShip extends SpaceShip {
-	public OtherSciFiSpaceShip (int tonnageAmount, String newName, String franchiseName) {
-	String name = newName;
-	int tonnage = tonnageAmount;
-	String Franchise = franchiseName;
-	}
-}
